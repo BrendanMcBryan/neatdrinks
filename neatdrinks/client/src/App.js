ff89aec85c3d05337657838fb7259dbd9b182111
Brendan McBryan <brendan.mcbryan@gmail.com>
2019-10-14 22:51:37 -0400

    latest at the end of wednesday

--- neatdrinks/client/src/App.js
+++ neatdrinks/client/src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Button from "@material-ui/core/Button";
+import Nav from "./components/Nav";
+
+// import Nav from "./components/Nav";
 
 function App() {
   return (
     <div>
+      <Nav></Nav>
       Hello World
       <Button variant="contained" color="primary">
         Hello World
