7dc60ce62c0086a2c181cee2a0a80b0e9a9b2b28
Brendan McBryan <brendan.mcbryan@gmail.com>
2019-10-14 20:24:00 -0400

    more initilaizing files

--- neatdrinks/client/src/App.js
+++ neatdrinks/client/src/App.js
@@ -1,12 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Button from '@material-ui/core/Button';
+import React from "react";
+import ReactDOM from "react-dom";
+import Button from "@material-ui/core/Button";
 
 function App() {
   return (
-    <Button variant="contained" color="primary">
+    <div>
       Hello World
-    </Button>
+      <Button variant="contained" color="primary">
+        Hello World
+      </Button>
+    </div>
   );
 }
 
