226d5ff8857f9782b181c62f63a70c4ad8ca8194
Brendan McBryan <brendan.mcbryan@gmail.com>
2019-10-16 20:21:00 -0400

    material theme connected

--- neatdrinks/client/src/App.js
+++ neatdrinks/client/src/App.js
@@ -1,19 +1,33 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import Button from "@material-ui/core/Button";
+// import ReactDOM from "react-dom";
+import { Button, Box, Container, TextField } from "@material-ui/core/";
 import Nav from "./components/Nav";
+
+
 
 // import Nav from "./components/Nav";
 
 function App() {
   return (
-    <div>
+    <Container maxWidth="sm">
       <Nav></Nav>
-      Hello World
+
       <Button variant="contained" color="primary">
         Hello World
       </Button>
-    </div>
+      <form noValidate autoComplete="off">
+        <TextField
+          id="standard-name"
+          label="Name"
+          // className={classes.textField}
+          // value={values.name}
+          // onChange={handleChange("name")}
+          margin="normal"
+        />
+      </form>
+
+      <Box color="primary.main">primary.main</Box>
+    </Container>
   );
 }
 
--- neatdrinks/client/src/index.js
+++ neatdrinks/client/src/index.js
@@ -1,11 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import orange from "@material-ui/core/colors/orange";
+import green from "@material-ui/core/colors/green";
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+const theme = createMuiTheme({
+  palette: {
+    primary: orange,
+    secondary: green
+  },
+  typography: {
+    useNextVariants: true
+  }
+});
 
+ReactDOM.render(
+  <MuiThemeProvider theme={theme}>
+    <App />{" "}
+  </MuiThemeProvider>,
+  document.querySelector("#root")
+);
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
