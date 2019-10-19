9241c7cb3a136470c26ef80076160440a1fc7435
Danny Falciani <Daniel@danny-falcianis-macbook-pro.local>
2019-10-19 13:41:31 -0400

    setup config.js that uses .env to connect to database. Setup the connection and created base user model

---
+++ neatdrinks/models/users.js
@@ -0,0 +1,23 @@
+module.exports = function(sequelize, DataTypes) {
+    const User = sequelize.define("User", {
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+        },
+        password: DataTypes.STRING,
+        user_type: DataTypes.INTEGER,
+        email: {
+            type: DataTypes.STRING,
+            unique: true,
+            validate: {
+              isEmail: true
+            }
+        },
+        address: DataTypes.STRING,
+        city: DataTypes.STRING,
+        state: DataTypes.STRING,
+        zip: DataTypes.STRING,
+    });
+    return User;
+  };
