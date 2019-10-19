466164538d05412eed2c7eb3a0e596e16cdb60bb
tuocha <kippon47@gmail.com>
2019-10-19 10:38:49 -0400

    replaced navbar at the landing page with logo

--- neatdrinks/client/src/pages/Landing/index.js
+++ neatdrinks/client/src/pages/Landing/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 // import { Button, Box, TextField } from "@material-ui/core/";
-import Nav from "../../components/Nav";
+// import Nav from "../../components/Nav";
+import Logo from "../../components/Logo";
 import LoginForm from "../../components/LoginForm";
 import DrinkCard from "../../components/DrinkCard";
 import NewAccountButton from "../../components/NewAccountButton";
@@ -10,7 +11,8 @@ import Footer from "../../components/Footer";
 export default function Landing() {
   return (
     <div>
-      <Nav />
+      {/* <Nav /> */}
+      <Logo />
       <LoginForm />
       <DrinkCard />
       <NewAccountButton />
