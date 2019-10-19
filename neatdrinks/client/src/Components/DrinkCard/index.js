eee42bba4a16e74c3a76b22e8a033f1e37b06ff3
Brendan McBryan <brendan.mcbryan@gmail.com>
2019-10-18 21:01:45 -0400

    added Drink Card and adjusted Landing

---
+++ neatdrinks/client/src/Components/DrinkCard/index.js
@@ -0,0 +1,74 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
+import Paper from "@material-ui/core/Paper";
+import Typography from "@material-ui/core/Typography";
+import ButtonBase from "@material-ui/core/ButtonBase";
+import CustomizedSlider from "../RatingSlider";
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1
+  },
+  paper: {
+    padding: theme.spacing(2),
+    margin: "auto",
+    maxWidth: 500
+  },
+  image: {
+    width: 128,
+    height: 128
+  },
+  img: {
+    margin: "auto",
+    display: "block",
+    maxWidth: "100%",
+    maxHeight: "100%"
+  }
+}));
+
+export default function ComplexGrid() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <Paper className={classes.paper}>
+        <Grid container spacing={2}>
+          <Grid item>
+            <ButtonBase className={classes.image}>
+              <img
+                className={classes.img}
+                alt="complex"
+                src="/images/neat-Logo-abstract.png"
+              />
+            </ButtonBase>
+          </Grid>
+          <Grid item xs={12} sm container>
+            <Grid item xs container direction="column" spacing={2}>
+              <Grid item xs>
+                <Typography gutterBottom variant="subtitle1">
+                  Piñacolada
+                </Typography>
+                <Typography variant="body2" gutterBottom>
+                If you're not into yoga, if you have half a brain
+                </Typography>
+                <Typography variant="body2" color="textSecondary">
+                  Rum • Pinapple Juice • Coconut
+                </Typography>
+              </Grid>
+              <Grid item>
+                <CustomizedSlider></CustomizedSlider>
+                <Typography variant="body2" style={{ cursor: "pointer" }}>
+                  Remove
+                </Typography>
+              </Grid>
+            </Grid>
+            <Grid item>
+              <Typography variant="subtitle1">89.5</Typography>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Paper>
+    </div>
+  );
+}
--- neatdrinks/client/src/pages/Landing/index.js
+++ neatdrinks/client/src/pages/Landing/index.js
@@ -1,28 +1,17 @@
 import React from "react";
-import { Button, Box, TextField } from "@material-ui/core/";
+// import { Button, Box, TextField } from "@material-ui/core/";
 import Nav from "../../components/Nav";
+import LoginForm from "../../components/LoginForm";
 import Footer from "../../components/Footer";
+import DrinkCard from "../../components/DrinkCard";
+
 
 function Landing() {
   return (
     <div>
       <Nav />
-
-      <Button variant="contained" color="primary">
-        Hello World
-      </Button>
-      <form noValidate autoComplete="off">
-        <TextField
-          id="standard-name"
-          label="Name"
-          // className={classes.textField}
-          // value={values.name}
-          // onChange={handleChange("name")}
-          margin="normal"
-        />
-      </form>
-
-      <Box color="primary.main">primary.main</Box>
+      <LoginForm />
+      <DrinkCard />
       <Footer />
     </div>
   );
