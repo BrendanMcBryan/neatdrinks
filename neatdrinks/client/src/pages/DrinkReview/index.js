566cbda9c3a6d0599cc717f0d0db47082c7fb085
brendan <brendan.mcbryan@gmail.com>
2019-10-20 17:40:42 -0400

    Drink Review Page in

--- neatdrinks/client/src/pages/DrinkReview/index.js
+++ neatdrinks/client/src/pages/DrinkReview/index.js
@@ -4,14 +4,17 @@ import { makeStyles } from "@material-ui/core/styles";
 
 // import Nav from "../../components/Nav";
 import LogoText from "../../components/LogoText";
-import RecipeFormDetail from "../../components/RecipeFormDetail";
+import NewDrinkButton from "../../components/NewDrinkButton";
+import RatingSlider from "../../components/RatingSlider";
 import Footer from "../../components/Footer";
 
 import Grid from "@material-ui/core/Grid";
 import Paper from "@material-ui/core/Paper";
 import Divider from "@material-ui/core/Divider";
 import Typography from "@material-ui/core/Typography";
-import DrinkCardList from "../../components/DrinkCardList";
+import DrinkReviewList from "../../components/DrinkReviewList";
+import { typography } from "@material-ui/system";
+import { Button } from "@material-ui/core";
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -19,25 +22,32 @@ const useStyles = makeStyles(theme => ({
     margin: 3
   }
 }));
-export default function User() {
+export default function DrinkReview() {
   const classes = useStyles();
   return (
     <div>
       <LogoText />
 
       <Paper className={classes.root}>
-        <Typography variant="h5" component="h1" color="primary">
-          Tom Cruise
+        <Typography variant="h5" component="h1" color="error">
+          Piñacolada
         </Typography>
-        <Typography variant="h7" component="h5" color="secondary">
-          …enjoys getting caught in the rain
+        <Typography align="right" variant="h7" component="h5" color="secondary">
+          from Seven Seas Bar
         </Typography>
       </Paper>
       <Paper className={classes.root}>
-        Your rated Drinks are below
+        Cocktail Average
+        <Typography align="right" variant="h2" component="h2" color="secondary">
+          89.5
+        </Typography>
       </Paper>
       <Paper className={classes.root}>
-        <DrinkCardList/>
+        Your Rating
+        <RatingSlider />
+      </Paper>
+      <Paper className={classes.root}>
+        <DrinkReviewList />
       </Paper>
       <Footer />
     </div>
